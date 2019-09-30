7c39de8bc8907194c43804ccec2cff35de8cae7e
Stanislav <stanislavk33@gmail.com>
2019-09-30 17:03:48 +0300

    Implement userLogin and UnitTests

---
+++ SonicState.Tests/ChordSequenceServiceShould.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonicState.Contracts.Repositories;
+using SonicState.Contracts.Services;
+using SonicState.Models.ViewModels;
+using SonicState.Repositories;
+using SonicState.Services;
+using System.Collections.Generic;
+
+namespace SonicState.Tests
+{
+    [TestClass()]
+    public class ChordSequenceServiceShould
+    {
+        private readonly ChordService chordService;
+        private readonly Mock<IChordUnitRepository> mockChordUnitRepository = new Mock<IChordUnitRepository>();
+        public ChordSequenceServiceShould()
+        {
+            this.chordService = new ChordService(mockChordUnitRepository.Object);
+        }
+        [TestMethod]
+        public void ReturnChordSequence ()
+        {
+            //arrange
+            //var mockValidator = new Mock<IChordUnitRepository>();
+           // var mockChordUnitRepository = new Mock<ChordUnitRepository>();
+           // var mockChordUnitDetails = new Mock<ChordUnitDetails>();
+
+            var mockChordUnitCollection = new List<ChordUnitDetails>()
+            {
+                new ChordUnitDetails
+                {
+                    AudioId = "1",
+                    Chord = "F#:7",
+                    Time = 2.33
+                },
+                new ChordUnitDetails
+                {
+                    AudioId = "1",
+                    Chord = "F#:7min",
+                    Time = 3.33
+                },
+                new ChordUnitDetails
+                {
+                    AudioId = "1",
+                    Chord = "F#:7",
+                    Time = 4.33
+                },
+                new ChordUnitDetails
+                {
+                    AudioId = "1",
+                    Chord = "F#:7min",
+                    Time = 5.33
+                },
+                new ChordUnitDetails
+                {
+                    AudioId = "1",
+                    Chord = "F#:7",
+                    Time = 6.33
+                }
+            };
+            string audioId = "1";
+
+           mockChordUnitRepository.Setup(x => x.GenerateChordSequence(It.IsAny<string>())).Returns(mockChordUnitCollection);
+           
+            ////act  
+            //var chordsequence = mockValidator.Object.GenerateChordSequence(audioId);
+            //IChordUnitRepository chordService = 
+            //chordService.GenerateChordSequence(audioId);
+            //mockValidator.Setup(m => m.GenerateChordSequence(It.IsAny<string>())).Returns((ICollection<ChordUnitDetails> c )=> chordsequence )
+            var result = chordService.GetChordSequence(audioId);
+
+            //assert
+            Assert.AreEqual(mockChordUnitCollection, result);
+
+        }
+
+}
+}
---
+++ SonicState.Validations/ValidationRules/LoginUserValidationRules.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using SonicState.Contracts;
+using SonicState.Contracts.Services;
+using SonicState.Models.BindingModels;
+
+namespace SonicState.Validations.ValidationRules
+{
+    public class LoginUserValidationRules : AbstractValidator<LoginUser>
+    {
+        private readonly IPasswordService passwordService;
+        private readonly IUserRepository userRepository;
+        public LoginUserValidationRules(
+             IPasswordService passwordService, IUserRepository userRepository)
+        {
+            this.passwordService = passwordService;
+            this.userRepository = userRepository;
+
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Must(Email => CheckIfEmailExists(Email)).WithMessage("There is no user with this email");
+
+            When(u => CheckIfEmailExists(u.Email) == true, () =>
+            {
+             RuleFor(u => u.Password)
+                .NotEmpty()
+                .Must((u, Password) => VerifyPassword(u.Email, Password)).WithMessage("Password doesn't match");
+            });
+                
+        }
+
+        private bool VerifyPassword(string email, string password)
+        {
+            var passwordHash = userRepository.GetPasswordHash(email);
+            if (passwordService.VerifyPassword(password, passwordHash)) return true;
+            return false;
+        }
+
+        private bool CheckIfEmailExists(string email)
+        {
+            if (userRepository.Exists(email)) { return true; }
+            return false;
+        }
+    }
+}
